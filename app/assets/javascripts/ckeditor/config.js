80ecbb2eb051ce275411860de933c559e37b4e19
Lucija Gregov <lgregov@gmail.com>
2016-06-20 23:34:34 +0100

    Added rich text formatting, added title to pins model, changed some wording, added production config for sending emails

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,13 @@
+// If you want to customize what user can see when creating text (underline, itallic, etc.) 
+// More info here  - https://github.com/galetahub/ckeditor 
+
+// CKEDITOR.editorConfig = function (config) {
+//   // ... other configuration ...
+
+//   config.toolbar_mini = [
+//     ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
+//   ];
+//   config.toolbar = "simple";
+
+//   // ... rest of the original config.js  ...
+// }
--- app/mailers/advert_mailer.rb
+++ app/mailers/advert_mailer.rb
@@ -1,14 +1,19 @@
+require 'digest/sha2' 
+
 class AdvertMailer < ApplicationMailer
+	# Prevent email to ending up in spam - later!
+	# default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@adverts.market"
+    
     default from: "info@adverts.market"
     layout 'mailer'
     
     def advert_email
       # @user = user
-      @url = 'http://example.com/login'
+      # @url = 'http://example.com/login'
       #mail(to: @user.email, subject: 'Welcome to My Awesome Site')
       
       # mail(to: 'lgregov@gmail.com ', subject: 'Welcome to My Awesome Site')
-      mail(to: 'donal.kerr@gmail.com ', subject: 'Welcome to My Awesome Site')
+      mail(to: 'lgregov@gmail.com ', subject: 'Welcome to Adverts Market')
     end
 
 end
